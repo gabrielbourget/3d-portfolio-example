dd0bf47f2c8baea16509a4ce657d107f3f6162e6
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-07-15 18:00:45 -0400

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,32 @@
-import { motion } from 'framer-motion';
+/* eslint-disable react/no-unescaped-entities */
+// import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import ComputersCanvas from './canvas/Computers';
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="2-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm &nbsp;
+            <span className="text-[#915eff]">Example Name</span>
+          </h1>
+
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Developer of 3D Visuals, user <br /> interfaces, and web applications
+          </p>
         </div>
       </div>
-    </div>
+
+      <ComputersCanvas />
+    </section>
   )
 }
 
